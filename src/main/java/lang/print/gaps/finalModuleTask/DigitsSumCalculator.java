38dd8c3680bdd504bb5d8164ff128c360e4ca485
Tamirlan4 <tamirlan_agubaev@mail.ru>
2022-10-19 15:41:51 +0600

    19 10 2022 15:41

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sum = 0;
+        String c =Integer.toString(number);
+        String k[] = c.split("");
+        for (int i = 0; i < k.length; i++) {
+            sum += Integer.parseInt(k[i]);
+        }
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int c = number % 10;
+        String r = ""+c;
+        c = number %100 /10;
+        r+=c;
+        c = number / 100;
+        r+=c;
+        c = Integer.parseInt(r);
+        System.out.println(c);
     }
 }
